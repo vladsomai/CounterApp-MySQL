f08dbe61cfdd4d28f4c8001c97d2e5f96382c6f5
Vlad Somai <58813056+vladsomai@users.noreply.github.com>
2022-03-08 09:41:51 +0200

    add comands for mysql in the first_run file

--- 0_first_run.sql
+++ 0_first_run.sql
@@ -5,12 +5,21 @@ ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '3ng1n33r
 flush privileges;
 
 #USE THE BELOW COMMAND TO CREATE THE PROCEDURES AFTER FIRST TIME INSTALL
-#SET SQL_SAFE_UPDATES = 0;
+SET SQL_SAFE_UPDATES = 0;
 
-#for changing mysql bind addresses
+#for changing mysql bind addresses(change the IP to 0.0.0.0 to allow all users to connect to the database)
 sudo nano /etc/mysql/mysql.conf.d/mysqld.cnf
 
 #allow all clients to connect to database with user root
-mysql> CREATE USER 'root'@'%' IDENTIFIED BY 'PASSWORD';
-mysql> GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
-mysql> FLUSH PRIVILEGES;
+CREATE USER 'root'@'%' IDENTIFIED BY 'PASSWORD';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+
+#command to be used when you want to execute a .sql file 
+mysql -u root -p3ng1n33r counterdb < 2_insert_into_projects.sql
+
+#open mysql cli:
+mysql -u root -p3ng1n33r counterdb 
+
+#close mysql cli:
+exit
