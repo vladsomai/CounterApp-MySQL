5310953acb44fd1f769bddea970e35a6e08d82cf
Vlad Somai <somaivlad@gmail.com>
2022-06-06 18:50:12 +0300

    add updateTemperature procedure

---
+++ 18_update_temperature.sql
@@ -0,0 +1,20 @@
+drop procedure if exists updateTemperature;
+
+delimiter //
+CREATE PROCEDURE updateTemperature(adapter_codeParam int, fixture_typeParam VARCHAR(30), temperatureParam int)
+BEGIN
+if (select exists(select * from Projects where adapter_code=adapter_codeParam and fixture_type=fixture_typeParam)) then
+update Projects 
+set temperature = temperatureParam
+where adapter_code = adapter_codeParam and fixture_type = fixture_typeParam;
+else
+    SIGNAL SQLSTATE '45000'
+      SET MESSAGE_TEXT = 'The adapter code does not exist with the specified fixture type!', MYSQL_ERRNO = 1001;
+end if;
+
+END;
+//
+delimiter ;
+
+call updateTemperature(101, "FCT", 32);
+
